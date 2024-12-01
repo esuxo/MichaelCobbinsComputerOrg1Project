dd7a7818f8d06a5c87c7ea35b210b4055b154e32
Michael Cobbins <mcobbins7@gmail.com>
2024-12-01 15:35:59 -0600

    Started main program logic with input prompt

--- program.asm
+++ program.asm
@@ -2,3 +2,9 @@
     prompt: .asciiz "Enter a number (N >= 25): "
     error: .asciiz "Illegal Number!\n"
     newline: .asciiz "\n"
+
+.text
+main:
+    li $v0, 4               # Print string
+    la $a0, prompt          # Load the prompt string
+    syscall
