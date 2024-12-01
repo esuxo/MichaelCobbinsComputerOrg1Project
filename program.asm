785336a237a1aaa71f1dede36e17f6064416af24
Michael Cobbins <mcobbins7@gmail.com>
2024-12-01 15:38:59 -0600

    Implemented input reading logic and stored input in

--- program.asm
+++ program.asm
@@ -8,3 +8,8 @@ main:
     li $v0, 4               # Print string
     la $a0, prompt          # Load the prompt string
     syscall
+   
+    li $v0, 5               #read integer
+    syscall
+    move $t0, $v0           #store input in $t0
+ 
